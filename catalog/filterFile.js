0bdde2b9bd5fee0a555b407ebd05893da69f87f2
avangard7 <razor6309@gmail.com>
2026-01-07 09:02:22 -0500

    lesson 12: create catalog

---
+++ catalog/filterFile.js
@@ -0,0 +1,30 @@
+const people = [{
+    name: "Ivan",
+    gender: "male",
+    age: 18
+               },
+               {
+    name: "Anna",
+    gender: "female",
+    age: 17
+               }, 
+               {
+    name: "Jessica",
+    gender: "female",
+    age: 36
+               }] 
+ 
+ 
+const adult = people.filter((person) => {
+	// const sex = detectSex(person); 
+	return person.age === 18 || person.age > 18; 
+});   
+
+
+console.log( adult );  
+
+
+/*                
+ const women = people.find((person) => {
+	return person.gender === "female"; 
+}); */ 
